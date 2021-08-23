68533dbacf4015e3abb8445f9b3722b4f06f25e3
jaanhio <jianhaot.89@gmail.com>
2021-08-23 23:20:04 +0800

    first cut at revamping collection page

---
+++ static/searchbar.js
@@ -0,0 +1,8 @@
+console.log("foo");
+
+const searchBar = document.getElementById("tag-searchbar");
+console.log(searchBar);
+
+
+// generation of options will be done via hugo template functions (resolved)
+// after which, js will be used to modify the elements' display upon selection
